a4bbac9323ff01d6a94d5060ba419b1cba274250
Mark Derricutt <mark.derricutt@smxemail.com>
2019-05-12 18:21:29 +1200

    git subrepo clone (merge) --branch=support/4.x https://github.com/HalBuilder/halbuilder-guava.git halbuilder-guava
    subrepo:
      subdir:   "halbuilder-guava"
      merged:   "79c98ae"
    upstream:
      origin:   "https://github.com/HalBuilder/halbuilder-guava.git"
      branch:   "support/4.x"
      commit:   "79c98ae"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

---
+++ halbuilder-guava/src/test/java/com/theoryinpractise/halbuilder/guava/InterfaceSatisfactionTest.java
@@ -0,0 +1,59 @@
+package com.theoryinpractise.halbuilder.guava;
+
+import com.google.common.base.Function;
+import com.theoryinpractise.halbuilder.api.ReadableRepresentation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+import com.theoryinpractise.halbuilder.xml.XmlRepresentationFactory;
+import org.testng.annotations.Test;
+
+import javax.annotation.Nullable;
+import java.io.InputStreamReader;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class InterfaceSatisfactionTest {
+
+    private RepresentationFactory representationFactory = new XmlRepresentationFactory();
+
+    public static interface IPerson {
+        Integer getAge();
+
+        Boolean getExpired();
+
+        Integer getId();
+
+        String getName();
+    }
+
+    public static interface INamed {
+        String name();
+    }
+
+    public static interface IJob {
+        Integer getJobId();
+    }
+
+    public static interface ISimpleJob {
+        Integer jobId();
+    }
+
+    public static interface INullprop {
+        String nullprop();
+    }
+
+    @Test
+    public void testFunctionalInterfaceSatisfaction() {
+
+        ReadableRepresentation representation = representationFactory.readRepresentation(RepresentationFactory.HAL_XML, new InputStreamReader(InterfaceSatisfactionTest.class.getResourceAsStream("/example.xml")));
+
+        String name = Representations.ifSatisfiedBy(representation, IPerson.class, new Function<IPerson, String>() {
+            public String apply(@Nullable IPerson iPerson) {
+                return iPerson.getName();
+            }
+        }).get();
+
+        assertThat(name).isEqualTo("Example Resource");
+
+    }
+
+}
