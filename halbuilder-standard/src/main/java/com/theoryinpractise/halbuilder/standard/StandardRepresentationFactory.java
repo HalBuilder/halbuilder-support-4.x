8362039d1c7b7a1c197f004641a9cbf3f3860e21
Mark Derricutt <mark.derricutt@smxemail.com>
2019-05-13 10:55:44 +1200

    Reformatted code and poms

--- halbuilder-standard/src/main/java/com/theoryinpractise/halbuilder/standard/StandardRepresentationFactory.java
+++ halbuilder-standard/src/main/java/com/theoryinpractise/halbuilder/standard/StandardRepresentationFactory.java
@@ -6,14 +6,12 @@ import com.theoryinpractise.halbuilder.json.JsonRepresentationWriter;
 import com.theoryinpractise.halbuilder.xml.XmlRepresentationReader;
 import com.theoryinpractise.halbuilder.xml.XmlRepresentationWriter;
 
-/**
- * Simple representation factory configured for JSON and XML usage.
- */
+/** Simple representation factory configured for JSON and XML usage. */
 public class StandardRepresentationFactory extends DefaultRepresentationFactory {
-    public StandardRepresentationFactory() {
-        withRenderer(HAL_JSON, JsonRepresentationWriter.class);
-        withReader(HAL_JSON, JsonRepresentationReader.class);
-        withRenderer(HAL_XML, XmlRepresentationWriter.class);
-        withReader(HAL_XML, XmlRepresentationReader.class);
-    }
+  public StandardRepresentationFactory() {
+    withRenderer(HAL_JSON, JsonRepresentationWriter.class);
+    withReader(HAL_JSON, JsonRepresentationReader.class);
+    withRenderer(HAL_XML, XmlRepresentationWriter.class);
+    withReader(HAL_XML, XmlRepresentationReader.class);
+  }
 }
